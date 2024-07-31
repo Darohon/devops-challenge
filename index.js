f90c8e62df5535c0b843f5a6497b1784350cebc1
aprotski <artjom.protski@gmail.com>
2024-07-31 11:49:46 +0300

    Added express and requests

--- index.js
+++ index.js
@@ -1,26 +1,67 @@
-//All tutorial Node.Js code examples are for reference only and shouldn't be used in production code as is. In production, a new new pipedrive.ApiClient() instance should be initialised separately for each request.
+
+const express = require('express');
+const expressApp = express();
+
 const pipedrive = require('pipedrive');
 const defaultClient = new pipedrive.ApiClient();
-
+const api = new pipedrive.DealsApi(defaultClient);
+    
 // Configure authorization by settings api key
 // PIPEDRIVE_API_KEY is an environment variable that holds real api key
-console.log(process.env.PIPEDRIVE_API_KEY);
 defaultClient.authentications.api_key.apiKey = process.env.PIPEDRIVE_API_KEY;
+
+expressApp.listen(3000, () => {
+    console.log('cool beans');
+});
+
+expressApp.get('/deals', async function (req, res) {
+    res.send(await getDeals());
+});
 
 async function getDeals() {
     try {
-        console.log('Sending request...');
-
-        const api = new pipedrive.DealsApi(defaultClient);
+        console.log('Sending GET request...');
 
         const response = await api.getDeals();
 
         console.log('Got deals successfully!', response);
+
+        return response;
+
     } catch (err) {
         const errorToLog = err.context?.body || err;
 
         console.log('Getting deals failed', errorToLog);
+
+        return 'Getting deals failed';
     }
 }
 
-getDeals();
+async function addDeal() {
+    try {
+        console.log('Sending POST request...');
+
+        const data = {
+            title: 'Deal of the century',
+            value: 10000,
+            currency: 'USD',
+            user_id: null,
+            person_id: null,
+            org_id: 1,
+            stage_id: 1,
+            status: 'open',
+            expected_close_date: '2022-02-11',
+            probability: 60,
+            lost_reason: null,
+            visible_to: 1,
+            add_time: '2021-02-11',
+        }
+        const response = await api.addDeal(data);
+
+        console.log('Deal was added successfully!', response);
+    } catch (err) {
+        const errorToLog = err.context?.body || err;
+
+        console.log('Adding failed', errorToLog);
+    }
+}
