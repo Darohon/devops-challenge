45d6591372b2a8cbccb06ec754926e457131e36c
aprotski <artjom.protski@gmail.com>
2024-08-01 14:38:27 +0300

    Added deal update

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ expressApp.get('/deals', async function (req, res) {
     res.send(await getDeals());
 });
 
+class Deal {
+    constructor(title, value, currency, user_id, person_id, org_id, stage_id, status, expected_close_date, probability, lost_reason, visible_to, add_time) {
+        this.title = title;
+        this.value = value;
+        this.currency = currency;
+        this.user_id = user_id;
+        this.person_id = person_id;
+        this.org_id = org_id;
+        this.stage_id = stage_id;
+        this.status = status;
+        this.expected_close_date = expected_close_date;
+        this.probability = probability;
+        this.lost_reason = lost_reason;
+        this.visible_to = visible_to;
+        this.add_time = add_time;
+    }
+}
+
 async function getDeals() {
     try {
         console.log('Sending GET request...');
@@ -37,25 +55,12 @@ async function getDeals() {
     }
 }
 
-async function addDeal() {
+async function addDeal(title, value, currency, user_id, person_id, org_id, stage_id, status, expected_close_date, probability, lost_reason, visible_to, add_time) {
     try {
         console.log('Sending POST request...');
 
-        const data = {
-            title: 'Deal of the century',
-            value: 10000,
-            currency: 'USD',
-            user_id: null,
-            person_id: null,
-            org_id: 1,
-            stage_id: 1,
-            status: 'open',
-            expected_close_date: '2022-02-11',
-            probability: 60,
-            lost_reason: null,
-            visible_to: 1,
-            add_time: '2021-02-11',
-        }
+        const data = new Deal(title, value, currency, user_id, person_id, org_id, stage_id, status, expected_close_date, probability, lost_reason, visible_to, add_time);
+
         const response = await api.addDeal(data);
 
         console.log('Deal was added successfully!', response);
@@ -65,3 +70,33 @@ async function addDeal() {
         console.log('Adding failed', errorToLog);
     }
 }
+
+async function updateDeal() {
+    try {
+        console.log('Sending request...');
+
+        const DEAL_ID = 1;  // id of the deal you want to update
+        const data = {
+            title: 'Deal of the century',
+            value: 20000,
+            currency: 'EUR',
+            user_id: null,
+            person_id: null,
+            org_id: 1,
+            stage_id: 2,
+            status: 'open',
+            expected_close_date: '2022-03-11',
+            lost_reason: null,
+            visible_to: 1,
+        }
+        const response = await api.updateDeal(DEAL_ID, data);
+
+        console.log(`Deal updated successfully!`, response);
+    } catch (err) {
+        const errorToLog = err.context?.body || err;
+
+        console.log('Deal update failed', errorToLog);
+    }
+}
+
+// addDeal('Deal of the century', 10000, 'USD', null, null, 1, 1, 'open', '2022-02-11', 60, null, 1, '2021-02-11');
